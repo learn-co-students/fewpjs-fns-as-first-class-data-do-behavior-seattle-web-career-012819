c2b740bd89ab08c5f746e8fedef54e22d3182c24
tiefengeist <sahirnambiar@gmail.com>
2019-03-11 15:21:03 -0700

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,18 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet(time) {
+  let h = parseInt((time.split(':')[0]))
+  if (h<12) {
+    return 'Good Morning'
+  }
+  else if ((h>12) && (h<17)) {
+    return "Good Afternoon"
+  }
+  else {return "Good Evening"}
+}
 /* Write your implementation of greet() */
+function displayMessage(greet) {
+  document.getElementById("greeting").textContent = greet;
+}
 /* Write your implementation of displayMessage() */
