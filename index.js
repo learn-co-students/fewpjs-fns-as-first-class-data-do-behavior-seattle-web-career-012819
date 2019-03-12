a0296019065d7c50d92ae7119645b342beeb93f6
Katie Carrel <katiecarrel@comcast.net>
2019-03-12 16:32:05 -0700

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
+function displayMessage(message) {
+  document.getElementById("greeting").textContent = message;
+}
 /* Write your implementation of greet() */
+function greet(timestring) {
+  const time = parseInt(timestring, 10);
+    if ( time < 12) return "Good Morning"
+    if ( time > 17) return "Good Evening"
+    return "Good Afternoon"
+  }
 /* Write your implementation of displayMessage() */
