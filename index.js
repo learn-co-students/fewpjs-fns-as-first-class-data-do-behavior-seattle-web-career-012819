ad3ef7ae30820cb8b7634e355b3c908b70dcc364
Madison Stankevich <madison.stankevich@flatironschool.com>
2019-03-11 17:09:00 -0700

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time){
+  let timeString = time.split(":")
+  let hour = parseInt(timeString[0])
+  if (hour <= 12){
+    return "Good Morning"
+  } else if (hour > 12 && hour < 17){
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(message){
+  document.getElementById('greeting').innerHTML = message;
+}
